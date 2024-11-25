277a70bfe9b9dac2c4837f4f4498728335201d6b
karth <karth@MONALUCK>
2024-11-24 18:10:32 -0600

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("539f65f1-c1b1-43af-ab83-59b3206b5b4e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver(); // You can change this to any other browser driver
+           
+        }
+
+        [TestMethod]
+        public void LoginTest_ValidCredentials()
+        {
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            Assert.IsTrue(driver.Url.Contains("facebook"), "sucess");
+        }
+
+        [TestMethod]
+        public void LogoutTest()
+        {
+            // Find username, password fields and login button, then interact with them
+            driver.FindElement(By.Id("email")).SendKeys("pavushettymounika@gmail.com");
+            driver.FindElement(By.Id("pass")).SendKeys("Mounik@93");
+            driver.FindElement(By.Id("u_0_9_XG")).Click();
+            driver.FindElement(By.PartialLinkText("mounika.pavushetty")).Click();
+            // Assert that login was successful (check if the dashboard is loaded)
+            Assert.IsTrue(driver.Url.Contains("mounika.pavushetty"), "Login failed, dashboard not found.");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            //driver.Quit(); // Close the browser after the test completes
+        }
+    }
+}
+
